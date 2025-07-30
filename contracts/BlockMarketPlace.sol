55ebe00f54fc97d7d38aed0c0423ec3851f5a59f
gregemax <gregemax700@gmail.com>
2025-07-30 13:30:36 +0100

    hmm

--- contracts/BlockMarketPlace.sol
+++ contracts/BlockMarketPlace.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 
 struct Listing{
     address owner;
@@ -24,6 +27,7 @@ abstract contract BlockMarketPlace {
 
 mapping (uint256 listid => Listing list) public idToListing;
 mapping (uint256 offerid => OfferDetails offer) public idToOffer;
+
     function listNft(Listing memory list) external {}
 
     function buyNft(uint256 listId) external payable {}
@@ -36,7 +40,20 @@ mapping (uint256 offerid => OfferDetails offer) public idToOffer;
 
     function cancelListing(uint256 listid) external {}
 
+}
+
+contract MarketPlace is  BlockMarketPlace{
 
 
-    
+function listNfts (Listing memory list) external {
+   require(list.price==0, "price not set");
+ 
+    idToListing[list.id] = list;
+}
+
+function buyNfts (uint256 listId) external payable {
+    Listing memory listed = idToListing[listId];
+    require(!listed.sold, "NFT already sold");
+    require(msg.value >= listed.price, "Insufficient payment");
+}
 }
